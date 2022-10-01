d2b4132d08a3c069ea941336145a93c1e9e8833c
Sergey <sergeykiselev@me.com>
2022-10-01 17:21:58 +0300

    add database. fix tests

--- src/test/java/ru/yandex/practicum/filmorate/integrations/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/integrations/FilmorateApplicationTests.java
@@ -5,14 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.MPA;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
 import ru.yandex.practicum.filmorate.storage.mpa.MPADbStorage;
-import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.util.Optional;
 
@@ -22,34 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
-    private final UserDbStorage userStorage;
-    private final FilmDbStorage filmDbStorage;
     private final GenreDbStorage genreDbStorage;
     private final MPADbStorage mpaDbStorage;
-
-    @Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = userStorage.getUserById(1L);
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1L)
-                );
-    }
-
-    @Test
-    public void testFindFilmById() {
-
-        Optional<Film> filmOptional = filmDbStorage.getFilmById(1L);
-
-        assertThat(filmOptional)
-                .isPresent()
-                .hasValueSatisfying(film ->
-                        assertThat(film).hasFieldOrPropertyWithValue("id", 1L)
-                );
-    }
 
     @Test
     public void testFindGenreById() {
